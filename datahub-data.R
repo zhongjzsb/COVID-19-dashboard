dd704e926a00982958b0b53d67c4fc90d15739ba
zhongjzsb <zhongjzsb@gmail.com>
2020-03-29 16:03:43 +0000

    change datahub-data.R

--- datahub-data.R
+++ datahub-data.R
@@ -9,9 +9,10 @@ print(json_data$resources$name)
 # print all tabular data(if exists any)
 for(i in 1:length(json_data$resources$datahub$type)){
     if(json_data$resources$datahub$type[i]=='derived/csv'){
-        path_to_file = json_data$resources$path[i]
-        data <- read.csv(url(path_to_file))
-        print(json_data$resources$name[i])
-        print(head(data))
+        if (json_data$resources$name[i] == 'time-series-19-covid-combined_csv') {
+            path_to_file = json_data$resources$path[i]
+            data <- fread(path_to_file)
+            break
+        }
     }
 }
